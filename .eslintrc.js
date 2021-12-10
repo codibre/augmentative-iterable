e15a064c11c80cb77b9130b7634205afe04a9756
Thiago Santos <tos.oliveira@gmail.com>
2021-12-10 18:25:00 -0300

    refactor: isolating some symbols

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   rules: {
     'default-case': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-magic-numbers': 'off'
   }
 }
